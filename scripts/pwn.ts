eabfa742202ae7436083b8e990d9fe9803a86934
JJ-8 <34778827+JJ-8@users.noreply.github.com>
2023-08-18 15:48:50 +0200

    Second account will deploy in local mode
    To simulate the CTF scenario.

--- scripts/pwn.ts
+++ scripts/pwn.ts
@@ -19,6 +19,8 @@ async function main() {
   // If running against CTF network, it is the first account in the list in hardhat.config.ts
   const account = (await ethers.getSigners())[0]
   console.log(`account: ${account.address}`)
+  // Note: when running on local, the second account will be used for deploying the contract to
+  // simulate the CTF scenario where someone else deploys the contract (see below)
 
   // ***********************************************************
   // Deploy any setup contracts and store ethers.js contract instances
@@ -30,7 +32,7 @@ async function main() {
   if (network.name == "ctf") {
     setup = await ethers.getContractAt("Setup", ensureEnvVar("CONTRACT_ADDRESS"))
   } else {
-    setup = await (await ethers.getContractFactory("Setup")).deploy()
+    setup = await (await ethers.getContractFactory("Setup", { signer: (await ethers.getSigners())[1] })).deploy()
   }
   await setup.waitForDeployment();
 
